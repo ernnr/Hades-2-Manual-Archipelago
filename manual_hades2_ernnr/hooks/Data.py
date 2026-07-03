bd29b652baa5f690f51b12a3ab61dd47f5968374
EthanRenner <eprenner87@gmail.com>
2026-07-03 16:52:42 -0500

    Add options for progressive arcana cards and weapons

--- manual_hades2_ernnr/hooks/Data.py
+++ manual_hades2_ernnr/hooks/Data.py
@@ -1,13 +1,19 @@
 from copy import deepcopy
 
 fear_levels = [0, 1, 2, 4, 8, 16, 32]
+progressive_items = {
+    "Arcana Cards": 3,
+    "Weapons and Aspects": 5
+}
 
 # called after the game.json file has been loaded
 def after_load_game_file(game_table: dict) -> dict:
     return game_table
+
 # called after the items.json file has been loaded, before any item loading or processing has occurred
 # if you need access to the items after processing to add ids, etc., you should use the hooks in World.py
 def after_load_item_file(item_table: list) -> list:
+    item_table = duplicate_progressive_items(item_table)
     return item_table
 
 # NOTE: Progressive items are not currently supported in Manual. Once they are,
@@ -34,6 +40,12 @@ def after_load_region_file(region_table: dict) -> dict:
 
 # called after the categories.json file has been loaded
 def after_load_category_file(category_table: dict) -> dict:
+    for category, _ in progressive_items.items():
+        category_table[f"Progressive {category}"] = {
+            "hidden": True,
+            "yaml_option": [f"progressive_{category.lower().replace(' ', '_')}_enabled"]
+        }
+
     for fear_level in fear_levels[1:]:  # Skip 0 Fear since it doesn't need an option
         category_table[f"{fear_level} Fear - Location Clears"] = {
             "hidden": True,
@@ -49,6 +61,19 @@ def after_load_category_file(category_table: dict) -> dict:
 def after_load_option_file(option_table: dict) -> dict:
     # option_table["core"] is the dictionary of modification of existing options
     # option_table["user"] is the dictionary of custom options
+    for category, _ in progressive_items.items():
+        option_table["user"][f"progressive_{category.lower().replace(' ', '_')}_enabled"] = {
+            "type": "Toggle",
+            "display_name": f"Progressive {category}",
+            "description": [
+                f"Adds progressive items for {category}.",
+                "Should only be enabled if using a custom save file that has all Weapons, Aspects, and Arcana Cards at level 0 or 1, and the resources necessary to upgrade them.",
+                "Should not be enabled if using a completed save file."
+                ],
+            "default": False,
+            "group": "Progressive Item Options"
+        }
+
     for fear_level in fear_levels[1:]:  # Skip 0 Fear since it doesn't need an option
         option_table["user"][f"locations_{fear_level}_fear_enabled"] = {
             "type": "Toggle",
@@ -71,6 +96,27 @@ def after_load_option_file(option_table: dict) -> dict:
 def after_load_meta_file(meta_table: dict) -> dict:
     return meta_table
 
+
+# Duplicates items in the "progressive_items" list, creating extra items with an extra category.
+def duplicate_progressive_items(item_table: list) -> list:
+    for category, count in progressive_items.items():
+        matching_items = []
+        for item in item_table:
+            categories = item.get("category")
+            if isinstance(categories, list) and category in categories:
+                matching_items.append(item)
+
+        if matching_items:
+            for item in matching_items:
+                name = item.get("name")
+
+                new_item = deepcopy(item)
+                new_item["name"] = f"Progressive - {name}"
+                new_item["category"].remove(category)
+                new_item["category"] += [f"Progressive {category}"]
+                new_item["count"] = count
+                item_table.append(new_item)
+    return item_table
 
 # Duplicates locations with the "Location Clears" category for each level of fear, creating new locations with modified names and requirements.
 def duplicate_location_clears(location_table: list) -> list:
