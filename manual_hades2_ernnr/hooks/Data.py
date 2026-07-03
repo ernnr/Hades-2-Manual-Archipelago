2a38aa69eefa6fb3909aa71eb8a922beba88040b
EthanRenner <eprenner87@gmail.com>
2026-07-03 11:57:32 -0500

    Update guardians to be duplicated for each fear level

--- manual_hades2_ernnr/hooks/Data.py
+++ manual_hades2_ernnr/hooks/Data.py
@@ -18,7 +18,62 @@ def after_load_progressive_item_file(progressive_item_table: list) -> list:
 # called after the locations.json file has been loaded, before any location loading or processing has occurred
 # if you need access to the locations after processing to add ids, etc., you should use the hooks in World.py
 def after_load_location_file(location_table: list) -> list:
-    # Duplicate "Location Clears" locations for each region
+    location_table = duplicate_location_clears(location_table)
+    location_table = duplicate_guardian_clears(location_table)
+    return location_table
+
+# called after the events.json file has been loaded, before any processing has occurred
+# If you need access to the events after processing, you should use the hooks in World.py
+def after_load_event_file(event_table: list) -> list:
+    return event_table
+
+# called after the regions.json file has been loaded, before any location loading or processing has occurred
+# if you need access to the locations after processing to add ids, etc., you should use the hooks in World.py
+def after_load_region_file(region_table: dict) -> dict:
+    return region_table
+
+# called after the categories.json file has been loaded
+def after_load_category_file(category_table: dict) -> dict:
+    for fear_level in fear_levels[1:]:  # Skip 0 Fear since it doesn't need an option
+        category_table[f"{fear_level} Fear - Location Clears"] = {
+            "hidden": True,
+            "yaml_option": [f"locations_{fear_level}_fear_enabled"]
+        }
+        category_table[f"{fear_level} Fear - Guardians"] = {
+            "hidden": True,
+            "yaml_option": [f"guardians_{fear_level}_fear_enabled"]
+        }
+    return category_table
+
+# called after the categories.json file has been loaded
+def after_load_option_file(option_table: dict) -> dict:
+    # option_table["core"] is the dictionary of modification of existing options
+    # option_table["user"] is the dictionary of custom options
+    for fear_level in fear_levels[1:]:  # Skip 0 Fear since it doesn't need an option
+        option_table["user"][f"locations_{fear_level}_fear_enabled"] = {
+            "type": "Toggle",
+            "display_name": f"Clear Locations with {fear_level} Fear",
+            "description": [f"Adds locations for clearing each location with {fear_level} Fear."],
+            "default": False,
+            "group": "Location Clear Options"
+        }
+        option_table["user"][f"guardians_{fear_level}_fear_enabled"] = {
+            "type": "Toggle",
+            "display_name": f"Defeat Guardians with {fear_level} Fear",
+            "description": [f"Adds locations for defeating each Guardian with {fear_level} Fear."],
+            "default": False,
+            "group": "Guardian Options"
+        }
+    return option_table
+
+# called after the meta.json file has been loaded and just before the properties of the apworld are defined. You can use this hook to change what is displayed on the webhost
+# for more info check https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/world%20api.md#webworld-class
+def after_load_meta_file(meta_table: dict) -> dict:
+    return meta_table
+
+
+# Duplicates locations with the "Location Clears" category for each level of fear, creating new locations with modified names and requirements.
+def duplicate_location_clears(location_table: list) -> list:
     matching_locations = []
     for location in location_table:
         categories = location.get("category")
@@ -44,7 +99,8 @@ def after_load_location_file(location_table: list) -> list:
                     new_location = deepcopy(location)
                     new_location["name"] = f"{fear_level} Fear - {region} Location #{index}"
                     new_location["requires"] = f"{{ItemValue(Fear:{fear_level})}}"
-                    new_location["category"] += f"{fear_level} Fear - Location Clears"
+                    if fear_level > 0:
+                        new_location["category"] += [f"{fear_level} Fear - Location Clears"]
                     location_table.append(new_location)
 
             # Remove base location
@@ -52,40 +108,31 @@ def after_load_location_file(location_table: list) -> list:
 
     return location_table
 
-# called after the events.json file has been loaded, before any processing has occurred
-# If you need access to the events after processing, you should use the hooks in World.py
-def after_load_event_file(event_table: list) -> list:
-    return event_table
+# Duplicates locations with the "Guardians" category for each level of fear, creating new locations with modified names and requirements.
+def duplicate_guardian_clears(location_table: list) -> list:
+    matching_locations = []
+    for location in location_table:
+        categories = location.get("category")
+        if isinstance(categories, list) and "Guardians" in categories:
+            matching_locations.append(location)
 
-# called after the regions.json file has been loaded, before any location loading or processing has occurred
-# if you need access to the locations after processing to add ids, etc., you should use the hooks in World.py
-def after_load_region_file(region_table: dict) -> dict:
-    return region_table
+    if matching_locations:
+        for location in matching_locations:
 
-# called after the categories.json file has been loaded
-def after_load_category_file(category_table: dict) -> dict:
-    for fear_level in fear_levels:
-        category_table[f"{fear_level} Fear - Location Clears"] = {
-            "hidden": True,
-            "yaml_option": [f"locations_{fear_level}_fear_enabled"]
-        }
-    return category_table
+            # Get the original name to construct the new name
+            name = location.get("name")
 
-# called after the categories.json file has been loaded
-def after_load_option_file(option_table: dict) -> dict:
-    # option_table["core"] is the dictionary of modification of existing options
-    # option_table["user"] is the dictionary of custom options
-    for fear_level in fear_levels:
-        option_table["user"][f"locations_{fear_level}_fear_enabled"] = {
-            "type": "Toggle",
-            "display_name": f"Clear Locations with {fear_level} Fear",
-            "description": [f"Adds locations for clearing each location with {fear_level} Fear."],
-            "default": False,
-            "group": "Location Clear Options"
-        }
-    return option_table
+            # For each level of fear, create a new location with the same region and categories, but with a modified name and requirement
+            for fear_level in fear_levels:
+                new_location = deepcopy(location)
+                new_location["name"] = f"{fear_level} Fear - {name}"
+                new_location["requires"] = f"{{ItemValue(Fear:{fear_level})}}"
+                if fear_level > 0:
+                    new_location["category"] += [f"{fear_level} Fear - Guardians"]
+                    new_location.pop("place_item", None)  # Remove place_item for fear levels greater than 0, since Gates are only required for fear 0
+                location_table.append(new_location)
 
-# called after the meta.json file has been loaded and just before the properties of the apworld are defined. You can use this hook to change what is displayed on the webhost
-# for more info check https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/world%20api.md#webworld-class
-def after_load_meta_file(meta_table: dict) -> dict:
-    return meta_table
+            # Remove base location
+            location_table.remove(location)
+
+    return location_table
